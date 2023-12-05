dc9f55143e9d29dc2a88b7a455f41cc91631e4ff
Franziska Steger <franziska.steger.tu@gmail.com>
2023-12-05 12:14:06 +0000

    created option for character not based on outcasts

--- got_survival/ml_logic/model_houses.py
+++ got_survival/ml_logic/model_houses.py
@@ -18,14 +18,18 @@ def houses_model_train():
     outcasts_y = outcasts['class']
     not_outcasts_X = not_outcasts.drop(columns='class')
     not_outcasts_y = not_outcasts['class']
+    X = house.drop(columns='class')
+    y = house['class']
 
     # Initiate models
     model_outcasts = KNeighborsClassifier(n_neighbors=1)
     model_not_outcasts = KNeighborsClassifier(n_neighbors=1)
+    model = KNeighborsClassifier(n_neighbors=1)
 
     # Train models
     model_outcasts.fit(outcasts_X, outcasts_y)
     model_not_outcasts.fit(not_outcasts_X, not_outcasts_y)
+    model.fit(X, y)
 
     # Save models
     with open('got_survival/models_pickle/outcasts.pkl', 'wb') as file:
@@ -34,17 +38,26 @@ def houses_model_train():
     with open('got_survival/models_pickle/not_outcasts.pkl', 'wb') as file:
         pickle.dump(model_not_outcasts, file)
 
-def houses_model_predict(X:pd.DataFrame) -> str:
+    with open('got_survival/models_pickle/houses.pkl', 'wb') as file:
+        pickle.dump(model, file)
+
+def houses_model_predict(X:pd.DataFrame, based_on_outcasts:bool=True) -> str:
     '''
     Predicts the house for a given character
     '''
-    # Checks for 'outcast' and uses correct model, returns prediction
-    if X['outcast'][0]:
-        model_outcasts = pickle.load(open('got_survival/models_pickle/outcasts.pkl', 'rb'))
-        return model_outcasts.predict(X)
+    if based_on_outcasts:
+        # Checks for 'outcast' and uses correct model, returns prediction
+        if X['outcast'][0]:
+            model_outcasts = pickle.load(open('got_survival/models_pickle/outcasts.pkl', 'rb'))
+            return model_outcasts.predict(X)
 
-    model_not_outcasts = pickle.load(open('got_survival/models_pickle/not_outcasts.pkl', 'rb'))
-    return model_not_outcasts.predict(X)[0]
+        model_not_outcasts = pickle.load(open('got_survival/models_pickle/not_outcasts.pkl', 'rb'))
+        return model_not_outcasts.predict(X)[0]
+
+    model = pickle.load(open('got_survival/models_pickle/houses.pkl', 'rb'))
+    return model.predict(X)[0]
+
+
 
 
 ###########################
@@ -64,6 +77,6 @@ X = pd.DataFrame.from_dict(new_character)
 
 if __name__ == '__main__':
 
-    # houses_model_train()
+    houses_model_train()
     # print(houses_model_predict(X))
     pass
