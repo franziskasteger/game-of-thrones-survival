8056e856beb66818e9c5fc51a60d4f6926b2e1d1
Franziska Steger <franziska.steger.tu@gmail.com>
2023-12-06 12:05:38 +0000

    season prediction in app

--- got_survival/interface/main.py
+++ got_survival/interface/main.py
@@ -45,45 +45,6 @@ def death_pred_RF(new_character:pd.DataFrame) -> int:
     return death_pipe.predict(new_character)[0] # Return prediction
 
 
-# Logistic regression
-def death_train() -> None:
-    '''
-    Will train a logistic regression pipeline to predict whether a character
-    survives or not.
-    '''
-    X, y = death_x_and_y() # Import data and split it into features and target
-    # Train-test-split with random state so that it is the same split for evaluating
-    # X_train, X_test, y_train, y_test = death_split_train(X, y)
-    pipe = death_create_pipeline() # Create pipeline with preprocessor and model
-    pipe.fit(X, y.values.ravel()) # Fit pipeline
-
-    with open("got_survival/models_pickle/death_model.pkl", "wb") as file:
-        pickle.dump(pipe, file) # Save trained model for evaluation and prediction
-
-def death_evaluate() -> float:
-    '''
-    Evaluate the trained logistic regression pipeline.
-    '''
-    # Load trained model
-    death_pipe = pickle.load(open("got_survival/models_pickle/death_model.pkl", "rb"))
-    X, y = death_x_and_y() # Import data
-    # Train-test-split with random state so that it is the same split as training
-    X_train, X_test, y_train, y_test = death_split_train(X, y)
-    y_pred = death_pipe.predict(X_test) # predict X_test with pipeline
-    score = death_f1_macro_score(y_test, y_pred) # calculate score
-    # maybe save the score somewhere?
-    return score
-
-def death_pred(new_character:pd.DataFrame) -> int:
-    '''
-    Use the fitted logistic regression pipeline to predict whether a new
-    character survives or not.
-    '''
-    # Load model
-    death_pipe = pickle.load(open("got_survival/models_pickle/death_model.pkl", "rb"))
-    return death_pipe.predict(new_character)[0] # Return prediction
-
-
 # Time of death
 def episode_train() -> None:
     '''
@@ -139,17 +100,9 @@ new_X = pd.DataFrame.from_dict(test)
 
 if __name__ == '__main__':
 
-    # print(f'------------------------------------------------------')
-    # print(f'-----------------Random Forest-------------------------')
     # death_train_RF()
     # print(death_evaluate_RF())
     # print(death_pred_RF(new_X))
-    # print(f'------------------------------------------------------')
-
-    # print(f'-----------------Logistic Regression-------------------------')
-    # death_train()
-    # print(death_evaluate())
-    # print(death_pred(new_X))
 
     # episode_train()
     # print(episode_evaluate())
