f970473b3718d0619f53b54afdb8bafe9c2a4f94
Franziska Steger <franziska.steger.tu@gmail.com>
2023-12-07 20:08:30 +0000

    turned picutres back on

--- streamlit_app.py
+++ streamlit_app.py
@@ -337,8 +337,8 @@ def run():
 
             my_bar.progress(50)
             try:
-                import time; time.sleep(5)
-                raise
+                # import time; time.sleep(5)
+                # raise
                 img_alive, filename_alive = create_image(character, age,
                                                          st.session_state["story"])
             except:
